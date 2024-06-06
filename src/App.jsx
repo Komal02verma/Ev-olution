24ebcf7c21269c2de577e8a6a23afa92697c3dee
Komal02verma <verma.komal0296@gmail.com>
2024-06-06 15:56:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Background from "./Component/Background/Background";
+import NavBar from "./Component/NavBar/NavBar";
+import Hero from "./Component/Hero/Hero";
+
+const App = () => {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
+    }, 3000);
+  }, []);
+  
+  return (
+    <>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <NavBar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </>
+  );
+};
+
+export default App;
